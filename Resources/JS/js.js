b279a53660add552469f8c6daae3b8b41dab22be
Stan <ksstan@gmail.com>
2022-06-03 11:44:06 +0800

    Final commit updated - Program functionality with code changed

--- Resources/JS/js.js
+++ Resources/JS/js.js
@@ -1,11 +1,10 @@
-alert("Welcome to the Dice Rolling Program that will roll a random number between 1-6, 1-12 and 1-20 respectively..");
+alert("Welcome to the Dice Rolling Program that will roll a random number between 1-6, 1-12 and 1-20..");
 
-var randomNumber1To6 = (Math.floor(Math.random() * 6)) + 1;
-var randomNumber1To12 = (Math.floor(Math.random() * 12)) + 1;
-var randomNumber1To20 = (Math.floor(Math.random() * 20)) + 1;
-
-function randomResults (rN1To6, rN1To12, rN1To20) {
-    alert("The random numbers generated are " + rN1To6 + ", " + rN1To12 + " & " + rN1To20 + " respectively.");
+function randomResults (randomNumbers) {
+    var randomResult = (Math.floor(Math.random() * randomNumbers)) + 1;
+    alert("The random number rolled is " + randomResult + ".");
 }
 
-randomResults (randomNumber1To6, randomNumber1To12, randomNumber1To20);
+randomResults (6);
+randomResults (12);
+randomResults (20);
